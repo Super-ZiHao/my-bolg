5a3bac4c304f754f399b7b4c99f268f401891d34
hzihhuang(黄子豪) <v_hzihhuang@tencent.com>
2022-04-19 09:48:24 +0800

    每日一问-模块划入动画

--- src/views/Everyday/EveryDayItem.tsx
+++ src/views/Everyday/EveryDayItem.tsx
@@ -1,15 +1,17 @@
-import { everyDayListType } from '@/utils/constants/everyDay'
+import { CSSProperties } from 'react'
 import { Collapse, Empty } from 'antd'
+import { everyDayListType } from '@/utils/constants/everyDay'
 
 type Props = {
   dataList: everyDayListType[]
   title: string
+  style?: CSSProperties
 }
 
-const EveryDayItem: React.FC<Props> = ({ dataList, title }) => {
+const EveryDayItem: React.FC<Props> = ({ dataList, title, style }) => {
   const { Panel } = Collapse
   return (
-    <div className="everyday-item card animate__animated animate__fadeInRight">
+    <div className="everyday-item card animate__animated animate__fadeInRight animate__delay-1s" style={style}>
       <div className='h-full' style={{ overflow: 'overlay' }}>
         <h1 className="everyday-title convex-text ff-kt ml-8">{ title }</h1>
         {dataList.length > 0 ? (
