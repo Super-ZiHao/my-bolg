26f974ca4db18d9ab5b6a764680e00c2a3d821d3
Mr.黄 <1106720577@qq.com>
2022-04-23 18:32:53 +0800

    更新配置项

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
       { find: 'component', replacement: path.resolve(__dirname, 'src/components') }
-    ]
-  }
+    ],
+  },
 });
